4c180ddbac1a970949452f2c8fb3dc2512f0a494
Abel Serra <aserra@fibex.local>
2025-01-20 09:09:43 -0400

    cambios

---
+++ .angular/cache/19.1.0/gregoriana/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,34 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  setAngularAppEngineManifest,
+  setAngularAppManifest
+} from "./chunk-DE4SHO4A.js";
+import "./chunk-J5GI37YW.js";
+import "./chunk-GXMTQ2XO.js";
+import "./chunk-QP66OYDI.js";
+import "./chunk-IIEZTSU4.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
